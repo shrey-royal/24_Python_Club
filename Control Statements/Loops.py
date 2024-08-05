45d6dd17dbbf41b550855b3e92476effbd039826
Shrey K <srkkadia@gmail.com>
2024-08-05 15:01:24 +0530

    Loops

---
+++ Control Statements/Loops.py
@@ -0,0 +1,53 @@
+'''
+Loop: Entry-Controlled Loop
+
+1. for: fixed iteration
+syntax:
+    for iterator_var in iterable:
+        # body
+
+2. while: unfixed iteration
+syntax:
+    while condition:
+        # body
+
+'''
+
+# range(start, stop-1, step) function
+
+# for i in range(65, 91):
+#     print(chr(i), end=' ')
+###############################################
+
+# for i in range(20):
+#     print(i, end=' ')
+###############################################
+
+# user_input = int(input("Enter the number of iterations: "))
+
+# for i in range(user_input):
+#     print(i, end=' ')
+###############################################
+
+# while loop
+# i=0
+# while(i < 5):
+#     print(i, end=' ')
+#     i+=1
+
+###############################################
+# choice = 1
+# while choice != 0:
+#     print("1. Tea\n2. Coffee\n3. Milkshake\n0. Exit")
+#     choice = int(input("Enter your choice: "))
+#     if choice > 0 and choice <= 3: print("Good!")
+#     elif choice == 0: break
+#     else: print("Invalid Choice! Try Again")
+
+###############################################
+while True:
+    print("1. Tea\n2. Coffee\n3. Milkshake\n0. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice > 0 and choice <= 3: print("Good!")
+    elif choice == 0: break
+    else: print("Invalid Choice! Try Again")
