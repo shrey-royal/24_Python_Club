c814126a976ad6ca9ffc64aab504258c04db8eab
Shrey K <srkkadia@gmail.com>
2024-10-16 15:14:40 +0530

    dictionary practice

--- Data Types/Dict_2/dict_methods.py
+++ Data Types/Dict_2/dict_methods.py
@@ -67,3 +67,11 @@ for v in D1.values():
         break
 
 print("Value is present" if found else "Value is not present")
+
+# Task:
+
+# Write a Python program to sum all the items in a dictionary.
+# Sample Input: 
+# my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
+# Sample Output: 
+# 293
